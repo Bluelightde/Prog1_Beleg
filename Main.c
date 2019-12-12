76a6d4b5f911deba645d6145c837706127109da9
Bluelightde <s78801@htw-dresden.de>
2019-12-12 12:27:16 +0100

    Anfang Liste

--- Main.c
+++ Main.c
@@ -1,23 +1,65 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include <time.h>
 #include <stdint.h>
 
-int main() {
-
-	struct Fahrradverleih
-	{
-		char Knr[5];
+struct fahrräder{
 		char Kennz[2];
 		char Art[12];
-		time_t Ausgabe[20];
-		
-
-
+		//char Reservierung[];
 	};
+
+struct verleih{
+		char Knr[5];
+		char Rnr[5];
+		struct verleih * nextVerleih; 
+};
+
+struct verleih *newVerleih;
+struct verleih *firstVerleih;
+struct verleih *currentVerleih;
+
+//Funktionen
+
+
+//int räder(anzahlRäder){
+
+
+//}
+
+
+int main() {
+
+	int anzahlRäder = 2;
+	
+	printf("\nHerzilch Willkomen im Fahrradverleih!\n\n");
+
+	printf("Reservierung\t - \t1\n");
+	printf("Stonierung\t - \t2\n");
+	printf("Ausgabe\t\t - \t3\n");
+	printf("Rücknahme\t - \t4\n\n");
+	printf("Treffen Sie eine Auswahl:\n\n");
+	
+	newVerleih = (struct verleih *) malloc(sizeof(struct verleih));
+
+	firstVerleih = newVerleih;
+	currentVerleih = firstVerleih;
+
+	puts("Kundennummer:");
+	scanf("%s", currentVerleih->Knr);
+	puts("Rechnungsnummer:");
+	scanf("%s", currentVerleih->Rnr);
+
+	currentVerleih->nextVerleih=NULL;
+
+	printf("--------------\n");
+	printf("Kundennummer: %s \n", currentVerleih->Knr);
+	printf("Rechnungsnummer: %s \n", currentVerleih->Rnr);
+
 	
 
 
-	printf("Hello World\n");
+	
 	return 0;
 }
 
